7619b3b436e35acce379a5ca22f9d70fbd6ba42e
Daniel A. Rodriguez-Delgado <daniel.a.rodriguez-delgado@boeing.com>
2025-02-11 13:59:58 -0800

    Revise local file path.

--- Chapter_09/Chapter_09.sql
+++ Chapter_09/Chapter_09.sql
@@ -58,7 +58,7 @@ CREATE TABLE pls_fy2018_libraries (
 );
 
 COPY pls_fy2018_libraries
-FROM 'C:\Users\Daniel\Documents\Programming Projects (GitHub)\Books\book-practical-sql\pls_fy2018_libraries.csv'
+FROM 'C:\Users\tg715c\Documents\Learning\book-practical-sql\Chapter_09\pls_fy2018_libraries.csv'
 WITH (FORMAT CSV, HEADER);
 
 CREATE INDEX libname_2018_idx ON pls_fy2018_libraries (libname);
@@ -166,11 +166,11 @@ CREATE TABLE pls_fy2016_libraries (
 );
 
 COPY pls_fy2017_libraries
-FROM 'C:\Users\Daniel\Documents\Programming Projects (GitHub)\Books\book-practical-sql\pls_fy2017_libraries.csv'
+FROM 'C:\Users\tg715c\Documents\Learning\book-practical-sql\Chapter_09\pls_fy2017_libraries.csv'
 WITH (FORMAT CSV, HEADER);
 
 COPY pls_fy2016_libraries
-FROM 'C:\Users\Daniel\Documents\Programming Projects (GitHub)\Books\book-practical-sql\pls_fy2016_libraries.csv'
+FROM 'C:\Users\tg715c\Documents\Learning\book-practical-sql\Chapter_09\pls_fy2016_libraries.csv'
 WITH (FORMAT CSV, HEADER);
 
 CREATE INDEX libname_2017_idx ON pls_fy2017_libraries (libname);
